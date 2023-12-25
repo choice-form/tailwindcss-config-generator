5271a8c327c5350478657b458a7a2bbe0a1b77c6
Wester <wester@choiceform.com>
2023-12-25 18:45:08 +0800

    Add UI dialog component and update header component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,13 +1,20 @@
 import classNames from "classnames";
 import {useTheme} from "next-themes";
+import {UiDialog} from "./ui";
+import {useState} from "react";
+import Link from "next/link";
 
 interface HeaderProps {}
 
 const Header = ({}: HeaderProps) => {
   const {theme, setTheme} = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="px-8 sticky top-0 shrink-0 bg-white/80 dark:bg-gray-900/80 z-40 backdrop-blur border-b border-gray-200 dark:border-gray-900">
+    <header
+      className="px-8 sticky top-0 shrink-0 bg-white/80
+      dark:bg-gray-900/80 z-40 backdrop-blur border-b border-gray-200 dark:border-gray-900"
+    >
       <div className="container mx-auto flex items-center gap-4 h-16">
         <div className="flex-1 flex items-center gap-4">
           <div className="bg-black text-white rounded p-1 dark:bg-white dark:text-gray-900">
@@ -16,9 +23,52 @@ const Header = ({}: HeaderProps) => {
           <h2 className="text-xl font-medium">@Choiceform/tailwindcss-config-generator</h2>
         </div>
         <div className="flex-0 flex items-center gap-4">
-          <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+          <button className="p-2" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
             <div className={classNames(theme === "dark" ? "ic-[sun]" : "ic-[moon-stars]")} />
           </button>
+
+          <Link
+            href={"https://github.com/choice-form/tailwindcss-config-generator"}
+            target="_blank"
+          >
+            <button className="p-2">
+              <div className="ic-[logo-github]" />
+            </button>
+          </Link>
+
+          <UiDialog
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            className="bg-white dark:bg-gray-900 rounded-lg shadow-lg p-8 w-[500px] relative
+            flex flex-col gap-4 justify-center items-center outline-none text-center"
+            trigger={
+              <button
+                className="flex items-center gap-2 rounded-lg bg-black text-white
+                py-2 px-3 text-sm dark:bg-white dark:text-black"
+              >
+                Sign in
+              </button>
+            }
+          >
+            <>
+              <button className="absolute top-4 right-4" onClick={() => setIsOpen(false)}>
+                <div className="ic-[e-remove]" />
+              </button>
+              <h1 className="text-4xl font-bold">Sign in</h1>
+              <p>Sign in to save and edit your custom color shades.</p>
+              <button
+                className="flex items-center gap-2 px-3 py-2 border border-gray-200
+                dark:border-gray-600 rounded-lg hover:bg-gray-100 hover:dark:bg-gray-700"
+              >
+                <div className="ic-[logo-github]" />
+                Sign in with Github
+              </button>
+              <span className="text-sm opacity-60">
+                By signing in, you ensure that your custom color shades are safely stored and can be
+                accessed and edited at any time.
+              </span>
+            </>
+          </UiDialog>
         </div>
       </div>
     </header>
--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -3,3 +3,4 @@ export {default as UiSwitch} from "./ui-switch";
 export {default as UiMenu} from "./ui-menu";
 export {default as UiPopover} from "./ui-popover";
 export {default as UiTabs} from "./ui-tabs";
+export {default as UiDialog} from "./ui-dialog";
