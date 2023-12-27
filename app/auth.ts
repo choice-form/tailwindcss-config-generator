61bcbeb3bd2720f9f0d534142f11a21ae5432e7b
张鑫 <zhang.xin@choiceform.com>
2023-12-27 17:53:33 +0800

    feat: add prisma

--- app/auth.ts
+++ app/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import { prisma } from './prisma';
 
 export const {
   handlers: { GET, POST },
@@ -11,7 +12,32 @@ export const {
       clientSecret: process.env.OAUTH_CLIENT_SECRET as string
     })
   ],
-  pages: {
-    signIn: '/sign-in'
-  }
+  // pages: {
+  //   signIn: '/sign-in'
+  // }
 });
+
+export async function getConfig() {
+  const session = await auth();
+  if (session && session.user && session.user.email) {
+    const res = await prisma.config.findFirst({
+      where: {
+        email: session.user.email
+      }
+    });
+    return res;
+  }
+}
+
+export async function saveConfig(config: string) {
+  const session = await auth();
+  if (session && session.user && session.user.email) {
+    const res = await prisma.config.create({
+      data: {
+        email: session.user.email,
+        content: config 
+      }
+    })
+    return res;
+  }
+}
